c487dd745e830cf38fa57b4ef87c3e2e909ad5f6
Ricardo Peraza <ricardo.peraza07@gmail.com>
2021-05-16 14:06:54 -0600

    pathfinding visualizer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { GridDrawComponent } from './grid-draw/grid-draw.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridDrawComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
